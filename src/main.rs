b1e693c66ba1e417d694f88e4ba28b575a0bbd8e
Abiria <abiria.dev@gmail.com>
2024-03-21 17:31:07 +0900

    feat: read from file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use base64ct::{Base64, Base64UrlUnpadded, Encoding};
+use std::{env::args, fs::read_to_string};
+
+use anyhow::anyhow;
+use base64ct::{Base64UrlUnpadded, Encoding};
 use sha2::{Digest, Sha256};
 
-fn main() {
-	let hash = Sha256::digest("hello world");
-	let result = Base64UrlUnpadded::encode_string(&hash);
-
-	println!("{}", &result[..16]);
+fn main() -> anyhow::Result<()> {
+	Ok(println!(
+		"{}",
+		&Base64UrlUnpadded::encode_string(&Sha256::digest(read_to_string(
+			args()
+				.nth(1)
+				.ok_or(anyhow!("No file provided"))?,
+		)?))[..16]
+	))
 }
